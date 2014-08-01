1f582b8c619ba784417693543b38cb6364c963e5
Guy Halperin & Sergii Mertsalov <student@epicodus.com>
2014-08-01 10:56:46 -0700

    add winning combos logic

--- lib/Board.rb
+++ lib/Board.rb
@@ -5,4 +5,34 @@ class Board
   def initialize
     @board = [['1'],['2'],['3'],['4'],['5'],['6'],['7'],['8'],['9']]
   end
+
+  def p1mark_space(space)
+    @board[space-1] = ['X']
+  end
+
+  def p2mark_space(space)
+    @board[space-1] = ['O']
+  end
+
+  def winning_combos
+    x_wins = [["X"], ["X"], ["X"]]
+    o_wins = [["O"], ["O"], ["O"]]
+    if @board.values_at(0,1,2) == x_wins || @board.values_at(0,1,2) == o_wins
+      return "game over"
+    elsif @board.values_at(0,4,8) == x_wins || @board.values_at(0,4,8) == o_wins
+      return "game Over"
+    elsif @board.values_at(0,3,6) == x_wins || @board.values_at(0,3,6) == o_wins
+      return "game over"
+    elsif @board.values_at(1,4,7) == x_wins || @board.values_at(1,4,7) == o_wins
+      return "game over"
+    elsif @board.values_at(2,5,8) == x_wins || @board.values_at(2,5,8) == o_wins
+      return "game over"
+    elsif @board.values_at(2,4,6) == x_wins || @board.values_at(2,4,6) == o_wins
+      return "game over"
+    elsif @board.values_at(3,4,5) == x_wins || @board.values_at(3,4,5) == o_wins
+      return "game over"
+    elsif @board.values_at(6,7,8) == x_wins || @board.values_at(6,7,8) == o_wins
+      return "game over"
+    end
+  end
 end
--- spec/ticTacToe_spec.rb
+++ spec/ticTacToe_spec.rb
@@ -15,12 +15,33 @@ describe 'Board' do
     expect(Player.players).to eq [test_player1, test_player2]
     expect(test_board.board).to eq [['1'],['2'],['3'],['4'],['5'],['6'],['7'],['8'],['9']]
   end
+
+  it 'marks a space with either X or O' do
+    test_board = Board.new
+    test_player1 = Player.new("Mark")
+    test_player2 = Player.new("Josh")
+    expect(test_board.p1mark_space(3)).to eq ['X']
+  end
+
+  it 'recognizes when 3 spots are marked in a row by the same player' do
+    test_board = Board.new
+    test_player1 = Player.new("Mark")
+    test_player2 = Player.new("Josh")
+    test_board.p1mark_space(0)
+    test_board.p2mark_space(7)
+    test_board.p1mark_space(1)
+    test_board.p1mark_space(3)
+    test_board.p1mark_space(2)
+    expect(test_board.winning_combos).to eq "game over"
+  end
 end
 
 describe 'Player' do
+
   before do
     Player.clear
   end
+
   it 'creates a player with a name' do
     test_player = Player.new("Mark")
     expect(test_player.name).to eq "Mark"
